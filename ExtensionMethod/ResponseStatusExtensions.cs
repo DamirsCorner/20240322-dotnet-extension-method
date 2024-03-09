75d0ac520276188c0e911ecc73cd4202d798b502
Damir Arh <damir.arh@gmail.com>
2024-03-09 13:53:23 +0100

    Fix null-state warning with attribute

--- ExtensionMethod/ResponseStatusExtensions.cs
+++ ExtensionMethod/ResponseStatusExtensions.cs
@@ -1,10 +1,11 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 
 namespace ExtensionMethod;
 
 public static partial class ResponseStatusExtensions
 {
-    public static bool IsSuccess(this ResponseStatus? status) =>
+    public static bool IsSuccess([NotNullWhen(true)] this ResponseStatus? status) =>
         status?.Code != null && SuccessCodeRegex().IsMatch(status.Code);
 
     [GeneratedRegex(@"^2\d{2}$")]
